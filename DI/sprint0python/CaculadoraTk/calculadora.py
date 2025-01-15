e39df89377f2afcf1b6851c80cba356e87d310ce
AlinaProject <alibogarin@gmail.com>
2025-01-15 13:40:15 +0100

    Calculadora 2.0....Lista!

--- DI/sprint0python/CaculadoraTk/calculadora.py
+++ DI/sprint0python/CaculadoraTk/calculadora.py
@@ -1,11 +1,76 @@
+
 from tkinter import *
 from math import *
 
-from operaciones import suma, resta, multiplicacion, division #llamo a los métodos
+
+
+
+#Defino la función para la entrada de números que aceptará una variable
+def btnClick(num):
+    global operador
+    operador = operador + str(num)
+    input_text.set(operador)
+
+#Defino la funcíon que realizará la operación
+def operation():
+    global operador
+    try:
+        opera=str(eval(operador))
+        input_text.set(opera)
+    except:
+        opera=("ERRORCITO")
+        operador=""
+
+#Defino la función que limpia la pantalla para realizar nuevos calculos
+def clear():
+    global operador
+    operador=("")
+    input_text.set("0")
 
 ventana = Tk()
 ventana.title("CALCULADORA")
 ventana.geometry("392x600") #Creo tamaño de la vetana
+
+# Variables para la función click_botton, importante ponelo debajo de la ventana para llamarla
+input_text = StringVar()
+operador =""
+
+
+
+
+
+#Tamaño de los botones
+ancho_boton = 11
+alto_boton =3
+
+#Defino el color del fondo de la calculadora, el color de los botobes esta definido en ella misma
+ventana.configure(background="SkyBlue1")
+
+#Creo los botones: el texto que se muestra en el boton, anchura y altura  y lo posiciono con place. LLamo tambien a la función de Click del botón
+Boton_0 = Button(ventana, text="0", width=ancho_boton, height= alto_boton, command=lambda:btnClick(0), bg="grey").place(x=17,y=180)
+Boton_1 = Button(ventana, text="1", width=ancho_boton, height= alto_boton, command=lambda:btnClick(1), bg="grey").place(x=107,y=180)
+Boton_2 = Button(ventana, text="2", width=ancho_boton, height= alto_boton, command=lambda:btnClick(2), bg="grey").place(x=197,y=180)
+Boton_3 = Button(ventana, text="3", width=ancho_boton, height= alto_boton, command=lambda:btnClick(3), bg="grey").place(x=287,y=180)
+Boton_4 = Button(ventana, text="4", width=ancho_boton, height= alto_boton, command=lambda:btnClick(4), bg="grey").place(x=17,y=240)
+Boton_5 = Button(ventana, text="5", width=ancho_boton, height= alto_boton, command=lambda:btnClick(5), bg="grey").place(x=107,y=240)
+Boton_6 = Button(ventana, text="6", width=ancho_boton, height= alto_boton, command=lambda:btnClick(6),  bg="grey").place(x=197,y=240)
+Boton_7 = Button(ventana, text="7", width=ancho_boton, height= alto_boton, command=lambda:btnClick(7), bg="grey").place(x=287,y=240)
+Boton_8= Button(ventana, text="8", width=ancho_boton, height= alto_boton, command=lambda:btnClick(8), bg="grey").place(x=17,y=300)
+Boton_9= Button(ventana, text="9", width=ancho_boton, height= alto_boton, command=lambda:btnClick(9), bg="grey").place(x=107,y=300)
+Boton_pi = Button(ventana, text="π", width=ancho_boton, height= alto_boton, command=lambda:btnClick("pi"), bg="grey").place(x=197,y=300)
+Boton_coma = Button(ventana, text=",", width=ancho_boton, height= alto_boton, command=lambda:btnClick(","), bg="grey").place(x=287,y=300)
+Boton_mas = Button(ventana, text="+", width=ancho_boton, height= alto_boton, command=lambda:btnClick("+"), bg="grey").place(x=17,y=360)
+Boton_menos = Button(ventana, text="-", width=ancho_boton, height= alto_boton, command=lambda:btnClick("-"), bg="grey").place(x=107,y=360)
+Boton_por= Button(ventana, text="*", width=ancho_boton, height= alto_boton, command=lambda:btnClick("*"), bg="grey").place(x=197,y=360)
+Boton_div= Button(ventana, text="/", width=ancho_boton, height= alto_boton, command=lambda:btnClick("/"), bg="grey").place(x=287,y=360)
+Boton_raiz = Button(ventana, text="√", width=ancho_boton, height= alto_boton, command=lambda:btnClick("sqrt("), bg="grey").place(x=17,y=420)
+Boton_par1= Button(ventana, text="(", width=ancho_boton, height= alto_boton, command=lambda:btnClick("("), bg="grey").place(x=107,y=420)
+Boton_par2 = Button(ventana, text=")", width=ancho_boton, height= alto_boton, command=lambda:btnClick(")"), bg="grey").place(x=197,y=420)
+Boton_mod = Button(ventana, text="%", width=ancho_boton, height= alto_boton, command=lambda:btnClick("%"),bg="grey").place(x=287,y=420)
+Boton_log = Button(ventana, text="ln", width=ancho_boton, height= alto_boton, command=lambda:btnClick("log("), bg="grey").place(x=17,y=480)
+Boton_lim = Button(ventana, text="C", width=ancho_boton, height= alto_boton, command=clear, bg="grey").place(x=107,y=480)
+Boton_pot = Button(ventana, text="EXP", width=ancho_boton, height= alto_boton, command=lambda:btnClick("**"), bg="grey").place(x=197,y=480)
+Boton_res = Button(ventana, text="=", width=ancho_boton, height= alto_boton, command=operation, bg="grey").place(x=287,y=480)
 
 #Salida de la calculadora
 salida = Entry(
@@ -13,75 +78,13 @@ salida = Entry(
     font=("Arial", 20, "bold"), #la fuente
     width=20, #ancho de la  vetana
     bd=22, #grasor del  borde
-    bg="pink", #blcolor
+    bg="light blue", #blcolor
     insertwidth=4, #la linea que parpadea dentro de la ventana del la calculadora
-    justify="center" # justificación
+    justify="left", # justificación de la linea que parpadea
+    textvariable=input_text #variable que muestra el texto en la ventana
 )
-salida.place(x=10,y=60)
-
-ancho_boton =11
-alto_boton =3
-
-#Creo los botones: el texto que se muestra en el boton, anchura y altura  y lo posiciono con place
-Boton0 = Button(ventana, text="0", width=ancho_boton, height= alto_boton).place(x=17,y=180)
-Boton1 = Button(ventana, text="1", width=ancho_boton, height= alto_boton).place(x=107,y=180)
-Boton2 = Button(ventana, text="2", width=ancho_boton, height= alto_boton).place(x=197,y=180)
-Boton3 = Button(ventana, text="3", width=ancho_boton, height= alto_boton).place(x=287,y=180)
-Boton4 = Button(ventana, text="4", width=ancho_boton, height= alto_boton).place(x=17,y=240)
-Boton5 = Button(ventana, text="5", width=ancho_boton, height= alto_boton).place(x=107,y=240)
-Boton6 = Button(ventana, text="6", width=ancho_boton, height= alto_boton).place(x=197,y=240)
-Boton7 = Button(ventana, text="7", width=ancho_boton, height= alto_boton).place(x=287,y=240)
-Boton8 = Button(ventana, text="8", width=ancho_boton, height= alto_boton).place(x=17,y=300)
-Boton9 = Button(ventana, text="9", width=ancho_boton, height= alto_boton).place(x=107,y=300)
-Boton9 = Button(ventana, text="π", width=ancho_boton, height= alto_boton).place(x=197,y=300)
-Boton9 = Button(ventana, text=",", width=ancho_boton, height= alto_boton).place(x=287,y=300)
-Boton9 = Button(ventana, text="+", width=ancho_boton, height= alto_boton).place(x=17,y=360)
-Boton9 = Button(ventana, text="-", width=ancho_boton, height= alto_boton).place(x=107,y=360)
-Boton9 = Button(ventana, text="*", width=ancho_boton, height= alto_boton).place(x=197,y=360)
-Boton9 = Button(ventana, text="/", width=ancho_boton, height= alto_boton).place(x=287,y=360)
-Boton9 = Button(ventana, text="√", width=ancho_boton, height= alto_boton).place(x=17,y=420)
-Boton9 = Button(ventana, text="(", width=ancho_boton, height= alto_boton).place(x=107,y=420)
-Boton9 = Button(ventana, text=")", width=ancho_boton, height= alto_boton).place(x=197,y=420)
-Boton9 = Button(ventana, text="%", width=ancho_boton, height= alto_boton).place(x=287,y=420)
-Boton9 = Button(ventana, text="ln", width=ancho_boton, height= alto_boton).place(x=17,y=480)
-Boton9 = Button(ventana, text="C", width=ancho_boton, height= alto_boton).place(x=107,y=480)
-Boton9 = Button(ventana, text="EXP", width=ancho_boton, height= alto_boton).place(x=197,y=480)
-Boton9 = Button(ventana, text="=", width=ancho_boton, height= alto_boton).place(x=287,y=480)
-
-
-
-
-
-
-
-"""
-def solicitar_numeros(): #creo otra función
-    num1 = int(input("1º número: "))
-    num2 = int(input("2º número: "))
-    return num1, num2
-
-def realizar_operacion(operacion, num1, num2): #utilizo los métodos dentro de una nueva función que llamaré más tarde
-    if operacion == "suma":
-        return suma(num1, num2)
-    elif operacion == "resta":
-        return resta(num1, num2)
-    elif operacion == "multiplicacion":
-        return multiplicacion(num1, num2)
-    elif operacion == "division":
-        if num2 == 0:
-            return "Error: División por cero"
-        else:
-            return division(num1, num2)
-    else:
-        return "Operación no reconocida"
-
-respuesta = 's'
-
-while respuesta.lower() == 's':
-    num1, num2 = solicitar_numeros()
-    operacion = input("¿Qué operación desea realizar? (\n- Suma \n- Resta \n- Multiplicación \n- Division ")
-    resultado = realizar_operacion(operacion.lower(), num1, num2)
-    print("El resultado es: " + str(resultado))
-    respuesta = input("¿Desea realizar otra operación (s/n)? ")
-"""
-ventana.mainloop() #Abrir ventana
+salida.place(x=22,y=65)
+#Limpiar pantalla
+clear()
+#Abrir ventana
+ventana.mainloop() 
